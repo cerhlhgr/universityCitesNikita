37c2b5bf8b5e0fd144c61bb0dab5993ca9a2e828
gurinnik <nikita2859.gylin.01@gmail.com>
2022-08-10 00:37:12 +0500

    test

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package universityCitesNikita
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Form)
+
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	})
+
+	server := http.Server{
+		Addr:    "2.59.42.22:8000",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
+}
