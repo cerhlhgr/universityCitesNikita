b3b3f3da6001a8a834943745303e61eeb52a95c7
gurinnik <nikita2859.gylin.01@gmail.com>
2022-08-13 22:20:06 +0500

    asd

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
+
+type vars struct {
+	Type   string `json:"type"`
+	Object map[string]interface{}
+}
 
 func main() {
 	log.Println("Запустился")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/nikitos", func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.PostFormValue("type"))
+		decoder := json.NewDecoder(r.Body)
+
+		params := vars{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		log.Println(params.Type)
+		switch params.Type {
+		case "message_new":
+			log.Println(params.Object)
+		}
 
 		w.WriteHeader(200)
-		_, err := w.Write([]byte("2d085acf"))
+		_, err = w.Write([]byte("ok"))
 		if err != nil {
 			log.Println(err.Error())
 			return
